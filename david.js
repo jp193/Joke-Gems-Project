779fe4396bcf890e8f8ba8321ceabfa8d9c7c504
dovid yarmush <yarmushda@gmail.com>
2023-05-15 14:11:22 -0400

    add css and display function

--- david.js
+++ david.js
@@ -3,9 +3,23 @@ const quoteBtn = document.querySelector('#quoteButton');
 quoteBtn.addEventListener('click', () => {
     fetch('https://api.quotable.io/quotes/random')
 .then(resp => resp.json())
-.then(data => {})
+.then(data => displayFetch(data))
 })
 
+// display results on the dom 
 function displayFetch(info) {
-    
+    const quoteDiv = document.querySelector('#quotes');
+    quoteDiv.textContent = ''
+
+    const quoteAuthor = document.createElement('h2');
+    const quote = document.createElement('p');
+
+    quoteAuthor.textContent = `Author: ${info[0].author}`;
+    quote.textContent = info[0].content;
+
+    quoteDiv.append(quoteAuthor, quote)
+
+    quoteDiv.classList = ''
+
+    console.log(info)
 }
