2b18a9d3c3e21d4378440a3798b1dff9c2182019
Jamal Portericker <jportericker@yahoo.com>
2023-05-15 12:16:31 -0400

    button edited

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-fetch('https://v2.jokeapi.dev/')
+fetch('https://v2.jokeapi.dev/joke/Any')
 .then(response => {
   if (response.ok) {
          console.log('it worked')
          return response.json()
        } throw new Error ('Request failed!')
      }, networkError => console.log(networkError.message))
- .then(data => {})
-
+  .then(data => {
+   
+ })
  .catch(error => console.log(error))
 
-// function getRandomFact() {
-//   $.ajax({
-//     url: '/api/v2/facts/random',
-//     method: 'GET',
-//     success: function(response) {
-//       $('#fact-container').text(response.fact);
-//     },
-//     error: function() {
-//       $('#fact-container').text('Failed to retrieve random fact.');
-//     }
-//   });
-// }
-
-// getRandomFact();
+ let subButton = document.getElementById('submit')
+subButton.style.background = 'linear-gradient(to left bottom, rgb(182, 95, 95), rgb(223, 4, 4))'
+ 
+subButton.addEventListener('mouseover', () => { 
+  subButton.style.background = 'linear-gradient(to left bottom, rgb(182, 95, 95), rgb(223, 4, 4))'
+  subButton.style.color = 'white'
+})
+subButton.addEventListener('mouseout', () => { 
+  subButton.style.background = 'linear-gradient(to left bottom, rgb(182, 95, 95), rgb(223, 4, 4))'
+  subButton.style.color = 'black'
+})
