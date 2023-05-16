fcb1bf482e0f1a9450e230bc0d6e9603cf092ce5
michoel <michoelr111@gmail.com>
2023-05-16 11:45:42 -0400

    add stash back to this file

--- michoel.js
+++ michoel.js
@@ -1,1 +1,15 @@
-console.log('hi');
+
+const adviceDiv = document.querySelector('#facts')
+const factButton = document.querySelector('#factButton')
+factButton.addEventListener('click', () => {
+    fetch('https://api.adviceslip.com/advice')
+.then(resp => resp.json())
+.then((advice) => {
+    console.log(advice.slip.advice)
+    renderFacts(advice) })
+})
+
+function renderFacts(advice) {
+   
+}
+
