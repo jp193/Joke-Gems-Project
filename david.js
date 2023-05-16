c793d8eedbac0367b4672055badf06f72a052dfd
dovid yarmush <yarmushda@gmail.com>
2023-05-16 11:16:41 -0400

    add css

--- david.js
+++ david.js
@@ -1,25 +1,30 @@
 const quoteBtn = document.querySelector('#quoteButton');
 
 quoteBtn.addEventListener('click', () => {
-    fetch('https://api.quotable.io/quotes/random')
-.then(resp => resp.json())
-.then(data => displayFetch(data))
+    fetchFn(displayFetch)
 })
 
 // display results on the dom 
 function displayFetch(info) {
-    const quoteDiv = document.querySelector('#quotes');
+    const quoteDiv = document.querySelector('#display');
     quoteDiv.textContent = ''
 
     const quoteAuthor = document.createElement('h2');
     const quote = document.createElement('p');
+    const div = document.createElement('div')
 
     quoteAuthor.textContent = `Author: ${info[0].author}`;
     quote.textContent = info[0].content;
 
-    quoteDiv.append(quoteAuthor, quote)
-
-    quoteDiv.classList = ''
-
+    quoteDiv.appendChild(div).append(quoteAuthor, quote)
+    div.setAttribute('class', 'innerDiv')
+    quoteDiv.classList = '';
     console.log(info)
+}
+
+// fetch function
+function fetchFn(callBack) {
+    fetch('https://api.quotable.io/quotes/random?limit=3')
+.then(resp => resp.json())
+.then(data => callBack(data))
 }
